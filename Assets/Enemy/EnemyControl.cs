8eb8940f3cfb1d7393e15ed25d9739ca5d7b5001
tvtom99 <tvtom99@yahoo.com.au>
2024-01-29 23:09:53 +0800

    i broke it just before so now actually fixed finished done get me out of here

--- Assets/Enemy/EnemyControl.cs
+++ Assets/Enemy/EnemyControl.cs
@@ -23,7 +23,7 @@ public class EnemyControl : MonoBehaviour
     [SerializeField]
     GameObject[] spawnPoints;
 
-    float timerTimerInit = 5f, floatTimer = 5f;
+    float timerTimerInit = 5f, timerTime = 5f;
 
     public bool gameOver = false;
 
@@ -46,7 +46,7 @@ public class EnemyControl : MonoBehaviour
         enemyList[0].GetComponent<NewEnemy>().SetPlayer(player);
         enemyList[0].GetComponent<NewEnemy>().SetBulletPrefab(bulletPrefab);
         Debug.Log("Enemy should be instantiated");*/
-        floatTimer = timerTimerInit;    //what are these variables
+        timerTime = timerTimerInit;    //what are these variables
         gameOver = false;
         enemyList = new GameObject[maxEnemies];
         SpawnEnemies();
